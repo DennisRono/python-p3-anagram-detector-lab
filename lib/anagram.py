531387e39d5a63e5b91e49dabba96f8ce4b6d896
Dennis Kibet <dennisrkibet@gmail.com>
2024-09-03 09:30:35 +0300

    passed the unit tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def match(self, word_list):
+        matches = []
+        for candidate in word_list:
+            if ''.join(sorted(candidate.lower())) == self.sorted_word:
+                matches.append(candidate)
+        return matches
